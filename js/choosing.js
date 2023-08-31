84353b42717a9f66c1141133c1423e86d3b5c9db
lrenais <bleuing@ewhain.net>
2023-08-31 16:25:10 +0900

    230831 choosing

---
+++ js/choosing.js
@@ -0,0 +1,14 @@
+const btn1=document.getElementById("choose_btn1");
+const btn2=document.getElementById("choose_btn2");
+
+//js에서 할 일 -> function(){}
+
+btn1.addEventListener('click',toStudyPage);
+btn2.addEventListener('click',toRestPage);
+
+function toStudyPage(){
+  window.location.href="./bldg_study.html";
+}
+function toRestPage(){
+  window.location.href="./bldg_rest.html";
+}
