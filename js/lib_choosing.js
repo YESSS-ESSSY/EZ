4822db577d3e6d8aba54bb0f007feaa858f2db91
lrenais <bleuing@ewhain.net>
2023-09-05 22:30:33 +0900

    0905-2230-lrenais-change

---
+++ js/lib_choosing.js
@@ -0,0 +1,19 @@
+const btn1=document.getElementById("choose_btn1");
+const btn2=document.getElementById("choose_btn2");
+const home_btn=document.getElementById("home_btn");
+
+//js에서 할 일 -> function(){}
+
+btn1.addEventListener('click',toStudyPage);
+btn2.addEventListener('click',toRestPage);
+home_btn.addEventListener('click',toHomePage);
+
+function toStudyPage(){
+  window.location.href="../pages/lib_study.html";
+}
+function toRestPage(){
+  window.location.href="../pages/lib_rest.html";
+}
+function toHomePage(){
+  window.location.href="../pages/first_page.html";
+}
